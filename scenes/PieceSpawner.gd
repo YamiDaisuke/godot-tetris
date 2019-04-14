94b8f796534d372d7e2db5609c7e37092d937f74
Franklin Cruz <daisuke.sysoft@gmail.com>
2019-04-14 18:21:15 -0400

    Pieces spawn based on fall instead of time

--- scenes/PieceSpawner.gd
+++ scenes/PieceSpawner.gd
@@ -2,17 +2,13 @@ extends Node2D
 
 const Piece = preload("res://scenes/Piece.tscn")
 
-export (float) var rate = 1
-
-
 func _ready():
-    $Timer.wait_time = rate
-    $Timer.start()
-
-func _process(delta):
-    pass    
-
-func _on_Timer_timeout():
+    pass
+    
+func spawn():
     var piece = Piece.instance()
     piece.shape = randi() % 7
     add_child(piece)
+    return piece
+
+    
