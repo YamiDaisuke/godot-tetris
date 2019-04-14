535c93cc2064899d53e7576dcce89ba49167d890
Franklin Cruz <daisuke.sysoft@gmail.com>
2019-04-14 19:13:59 -0400

    User input and block rotation fix

--- scenes/Piece.gd
+++ scenes/Piece.gd
@@ -1,6 +1,8 @@
 tool
 extends KinematicBody2D
 
+signal piece_have_fallen(piece)
+
 const BlockColors = preload("res://scenes/Block.gd").BlockColor
 
 enum Rotations { ZERO, NINETY, ONE_EIGTHY, TWO_SEVENTY }
@@ -24,7 +26,7 @@ const SHAPES = [
         Vector2(-0.5,0.5),
         Vector2(0.5,0.5),
         Vector2(1.5,0.5),
-    ],    
+    ],
     # Z
     [
         Vector2(-0.5,-0.5),
@@ -72,7 +74,7 @@ const SHAPES = [
 export(int, "I", "Z", "S", "T", "L", "J", "O") var shape = 0 setget _set_shape
 export (Rotations) var rotationPosition = Rotations.ZERO setget _set_rotation
 
-export (Vector2) var velocity = Vector2(0, 100)
+export (Vector2) var velocity = Vector2(0, 18)
 
 var stopped = false
 
@@ -91,16 +93,39 @@ func _ready():
 
 func _physics_process(delta):
     if !Engine.is_editor_hint() and !stopped:
-        var output = self.move_and_slide(velocity)
-        if output != velocity:
+
+        if Input.is_action_just_pressed("rotate_right"):
+            self.rotationPosition = self.rotationPosition + 1 % 4
+
+        if Input.is_action_just_pressed("rotate_left"):
+            self.rotationPosition = self.rotationPosition - 1 % 4
+
+
+        var speed = Vector2(velocity.x, velocity.y)
+
+        if Input.is_action_pressed("down"):
+            speed.y *= 10
+
+        var output = self.move_and_slide(speed)
+        if output.y != speed.y:
             self.stopped = true
+            self.emit_signal("piece_have_fallen", self)
+
+        if Input.is_action_just_pressed("left"):
+            #warning-ignore:return_value_discarded
+            self.move_and_slide(-Vector2(BLOCK_SIZE / delta, 0))
+
+        if Input.is_action_just_pressed("right"):
+            #warning-ignore:return_value_discarded
+            self.move_and_slide(Vector2(BLOCK_SIZE / delta, 0))
+
+
 
 
 func draw_shape(shape):
-    
     if self.blocks.empty():
         return
-    
+
     for i in range(self.blocks.size()):
         self.blocks[i].color = SHAPE_COLOR[shape]
         self.blocks[i].position = SHAPES[shape][i] * BLOCK_SIZE
@@ -108,8 +133,17 @@ func draw_shape(shape):
 
 func _set_rotation(new_rotation):
     rotationPosition = new_rotation
+
+    if self.blocks.empty():
+        return
+
+    for i in range(self.blocks.size()):
+        self.blocks[i].rotation = deg2rad(-90 * rotationPosition)
+
     self.rotation = deg2rad(90 * rotationPosition)
-    
+
+
+
 
 func _set_shape(new_shape: int):
     shape = new_shape
