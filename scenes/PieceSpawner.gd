87462c6354769a1607d38ea2f5cde0b55fb986b8
Franklin Cruz <daisuke.sysoft@gmail.com>
2019-04-15 21:12:51 -0400

    Basic game functions
    - Spawn pieces
    - Stack pieces
    - Clear full rows

--- scenes/PieceSpawner.gd
+++ scenes/PieceSpawner.gd
@@ -4,11 +4,11 @@ const Piece = preload("res://scenes/Piece.tscn")
 
 func _ready():
     pass
-    
+
 func spawn():
     var piece = Piece.instance()
     piece.shape = randi() % 7
     add_child(piece)
     return piece
 
-    
+
