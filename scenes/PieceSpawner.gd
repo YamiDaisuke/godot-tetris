cc1df5623c2ffa6b6d6ddb609ea3462edc8d4079
Franklin Cruz <fcruz@smartboxtv.com>
2019-04-22 23:30:47 -0400

    Return current and next piece

--- scenes/PieceSpawner.gd
+++ scenes/PieceSpawner.gd
@@ -2,12 +2,24 @@ extends Node2D
 
 const Piece = preload("res://scenes/Piece.tscn")
 
-func _ready():
-    pass
+var next_piece = null
 
 func spawn():
+    if next_piece == null:
+        self.next_piece = randi() % 7
+
     var piece = Piece.instance()
-    piece.shape = randi() % 7
+    piece.shape = self.next_piece
+    self.next_piece = randi() % 7
+    self.add_child(piece)
+
+    var output = {}
+    output['piece'] = piece
+    output['next'] = self.next_piece
+    return output
+
+
+
     add_child(piece)
     return piece
 
