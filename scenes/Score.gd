b8d016d30574c5701b368c0987967afcedf37b6d
Franklin Cruz <fcruz@smartboxtv.com>
2019-04-27 16:42:52 -0400

    UI Enhancements

--- scenes/Score.gd
+++ scenes/Score.gd
@@ -3,7 +3,7 @@ extends PanelContainer
 var score = 0
 var lines = 0
 
-var level = 0
+var level = 0 setget set_level
 
 # TODO: Force this array to have four elements
 export(Array, int) var line_score = [
@@ -13,8 +13,14 @@ export(Array, int) var line_score = [
     1200
 ]
 
+onready var level_label = $MarginContainer/VBoxContainer/LevelLabel
 onready var lines_label = $MarginContainer/VBoxContainer/HBoxContainer2/LabelLines
 onready var score_label = $MarginContainer/VBoxContainer/HBoxContainer/LabelScore
+
+func set_level(new_level:int):
+    level = new_level
+    level_label.text = "Level %d" % level
+
 
 func add_lines(new_lines:int):
     lines += new_lines
